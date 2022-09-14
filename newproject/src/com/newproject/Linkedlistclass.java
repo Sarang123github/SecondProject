c1d4ce45eebdb142864dc3a49642d64c304891b4
unknown <bhongsarang007@gmail.com>
2022-09-14 22:08:03 +0530

    done

---
+++ newproject/src/com/newproject/Linkedlistclass.java
@@ -0,0 +1,18 @@
+package com.newproject;
+
+import java.util.LinkedList;
+
+public class Linkedlistclass {
+
+	public static void main(String[] args) {
+	
+		LinkedList<Integer> list =new LinkedList<Integer>();
+		
+		list.add(10);
+		list.add(100);
+		list.add(1000);
+		
+		System.out.println(list);
+	}
+
+}
